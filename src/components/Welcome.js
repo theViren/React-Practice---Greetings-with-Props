95a9f47aa98f934a70ba87a731c52a49957b7a4c
mahziyar110 <mahziyar110@gmail.com>
2021-08-11 02:08:33 +0530

    new commit

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,9 @@
 // write code for Welcome component here
+function Welcome(props) {
+    return <>
+        <h1>Hey {props.name}!</h1>
+        <h2>Welcome to Newton School.</h2>
+    </>
+}
+
+export default Welcome;
